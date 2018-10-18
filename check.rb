47e4bfb030c74eb903030bf817ae76868c3ba9c6
Tadashi Saito <tad.a.digger@gmail.com>
2018-10-19 01:17:57 +0900

    possibly workable

--- check.rb
+++ check.rb
@@ -5,19 +5,25 @@ require 'rest-client'
 BUILD_HOOK = -ENV.fetch('BUILD_HOOK')
 FAMILY = -ENV.fetch('FAMILY')
 DATABASE_URL = -ENV.fetch('DATABASE_URL')
+
 BASE = -'https://fonts.googleapis.com/css?family='
+REQUEST_HEADERS = {
+  'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36' # Chromium 69
+}.freeze
+
+class FileDigest < ActiveRecord::Base; end
 
 def main
-  css = get(BASE + FAMILY)
-  digest = sha256(css)
+  resp = get(BASE + FAMILY)
+  digest = sha256(resp.body)
   if updated?(digest)
-    save(digest, Time.now)
+    save(digest)
     notify(BUILD_HOOK)
   end
 end
 
 def get(url)
-  RestClinet.get(url)
+  RestClient.get(url, REQUEST_HEADERS)
 end
 
 def sha256(css)
@@ -30,13 +36,15 @@ end
 
 def latest_digest
   ActiveRecord::Base.establish_connection(DATABASE_URL)
+  FileDigest.last&.digest
 end
 
-def save(digest, created_at)
+def save(digest)
+  FileDigest.create(digest: digest)
 end
 
 def notify(hook)
- RestClient.post(hook, '') # just for netlify
+  RestClient.post(hook, '') # just for netlify
 end
 
 main if $0 == __FILE__
