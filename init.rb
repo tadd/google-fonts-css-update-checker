bc0da601fba7ac1c42418c53d3e420e1efb929b1
Tadashi Saito <tad.a.digger@gmail.com>
2018-10-19 01:17:26 +0900

    implement create_table with AR

--- init.rb
+++ init.rb
@@ -1,7 +1,14 @@
 require 'active_record'
 
+DATABASE_URL = -ENV.fetch('DATABASE_URL')
+
 def create_table
-  # todo
+  ActiveRecord::Base.establish_connection(DATABASE_URL)
+  conn = ActiveRecord::Base.connection
+  conn.create_table(:file_digests) do |t|
+    t.string :digest
+    t.timestamps
+  end
 end
 
 create_table if $0 == __FILE__
